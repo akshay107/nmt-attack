fc281dc447e311735da43f4ca3ccbe45d3d17881
akshay107 <thegphenomenon@gmail.com>
2021-06-30 11:22:24 +0530

    added backup_models folder

---
+++ multilingual_nmt/backup_models/__init__.py
@@ -0,0 +1,9 @@
+__all__ = ['transformer', 'multitasknmt', 'shaped', 'transformer_shaped', 'langshare', 'transformer_langshare']
+
+from backup_models.transformer import Transformer
+from backup_models.transformer_shaped import TransformerShaped
+from backup_models.transformer_langshare import TransformerLangShare
+from backup_models.multitasknmt import MultiTaskNMT
+from backup_models.shaped import Shaped
+from backup_models.langshare import LangShare
+
